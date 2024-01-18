c1e56a0f1b645a57a4af80b874db6e749cb9af59
Noelle Smith <noellebsmith2@gmail.com>
2024-01-18 15:36:00 -0500

    Began creating the basis for a simple profile page

---
+++ src/main.py
@@ -0,0 +1,25 @@
+import asyncio
+import os, os.path
+import tornado.web
+import Index, Profile
+
+HTMLDIR = os.path.abspath(
+    os.path.join(os.path.dirname(__file__),
+                 "..","html"
+    )
+)
+
+def makeApp():
+    endpoints=[
+        ("/",Index.Handler),
+        ("/profile/.*",Profile.Handler)
+    ]
+    app = tornado.web.Application(endpoints,
+        static_path=HTMLDIR
+    )
+    app.listen(8000)
+    return app
+
+if __name__ == "__main__":
+    app = makeApp()
+    asyncio.get_event_loop().run_forever()
